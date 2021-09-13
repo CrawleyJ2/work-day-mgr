e61b3d65c839130d6d575ad7d6d31fbe4a506659
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-13 17:23:30 -0400

    items save to local storage tested

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
 const dateToday = moment().format('LLLL');
+const currentHour = moment().hours();
 // current date displays 
 $('#currentDay').html(dateToday);
+
+// set items to local storage when save button clicked
+$(document).ready(function() {
+  $('.saveBtn').on('click', function () {
+    var taskText = $(this).siblings('.description').val();
+    var taskTime = $(this).parent().attr('id');
+    
+    console.log(taskText, taskTime);
+
+    // set to localstorage
+    localStorage.setItem(taskTime, taskText)
+  })
+})
